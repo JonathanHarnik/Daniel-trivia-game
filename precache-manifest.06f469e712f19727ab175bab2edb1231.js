dc484d65db71fffa19f94253d42be1bd72b06c71
Jonathan <jonathan.harnik@gmail.com>
2020-10-26 16:03:37 +0200

    Updates

---
+++ precache-manifest.06f469e712f19727ab175bab2edb1231.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "545b300261fa64fbbdf4c15615483051",
+    "url": "/Daniel-trivia-game/index.html"
+  },
+  {
+    "revision": "1c66b37f9b331976c8c0",
+    "url": "/Daniel-trivia-game/static/css/main.ea68a5e2.chunk.css"
+  },
+  {
+    "revision": "9412b19a1f9c7f1d1638",
+    "url": "/Daniel-trivia-game/static/js/2.5fbbebd9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Daniel-trivia-game/static/js/2.5fbbebd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c66b37f9b331976c8c0",
+    "url": "/Daniel-trivia-game/static/js/main.b0f7a3d8.chunk.js"
+  },
+  {
+    "revision": "c53e1d7a82ab217cbbcb",
+    "url": "/Daniel-trivia-game/static/js/runtime-main.0a4839f0.js"
+  },
+  {
+    "revision": "db601dee0878b199e8fa2b5ff4b9c05c",
+    "url": "/Daniel-trivia-game/static/media/Crown.db601dee.jpeg"
+  },
+  {
+    "revision": "81d0f92292fd6f781d4ad4f87cd396f6",
+    "url": "/Daniel-trivia-game/static/media/bear.81d0f922.jpeg"
+  },
+  {
+    "revision": "91c665a6de8e745f20282d442559340f",
+    "url": "/Daniel-trivia-game/static/media/daniel1.91c665a6.jpg"
+  },
+  {
+    "revision": "c8c62d2b5dee75b3a9493f25ce655668",
+    "url": "/Daniel-trivia-game/static/media/daniel_laugh.c8c62d2b.jpg"
+  },
+  {
+    "revision": "c3d50a0481d51115f839f8e8ab87f02b",
+    "url": "/Daniel-trivia-game/static/media/daniel_sexy.c3d50a04.jpg"
+  },
+  {
+    "revision": "58c8b87fda5e1536f01032c0bf6b33fa",
+    "url": "/Daniel-trivia-game/static/media/us.58c8b87f.jpeg"
+  }
+]);
